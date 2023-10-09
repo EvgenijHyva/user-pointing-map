721f614fb4e27b5cf034dd964208bc1a849ececc
Evgeny <evgeny.hyvarinen@tietoevry.com>
2023-10-09 09:55:31 +0300

    serializers and views fixes

--- geobackend/map_app/serializer.py
+++ geobackend/map_app/serializer.py
@@ -3,9 +3,14 @@ from rest_framework import serializers
 from .models import Location
 from users.serializer import BasicUserSerializer
 
-class LocationSerializer(serializers.ModelSerializer):
-	user = BasicUserSerializer()
+class OwnedLocationsSerializer(serializers.ModelSerializer):
+	owner = BasicUserSerializer()
 
 	class Meta:
 		model = Location
 		fields = "__all__"
+
+class LocationSerializer(serializers.ModelSerializer):
+		class Meta:
+			model = Location
+			fields = "__all__"
--- geobackend/map_app/views.py
+++ geobackend/map_app/views.py
@@ -3,18 +3,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 
-from .serializer import LocationSerializer
+from .serializer import LocationSerializer, OwnedLocationsSerializer
 from .models import Location
 
 
 class LocationView(APIView):
 	def get(self, request: Request):
 		locations = Location.objects.all()
-		return Response(locations)
+		serializer = OwnedLocationsSerializer(locations, many=True)
+		print(serializer.data)
+		return Response(serializer.data)
 	
 	def post(self, request: Request):
 		data=request.data
-		print("post data:",data)
+		data["owner"] = request.user.id
 		serializer = LocationSerializer(data=data)
 		if serializer.is_valid(raise_exception=True):
 			serializer.save()
