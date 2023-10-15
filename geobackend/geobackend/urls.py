30f9546143f29307372a005af322179978411684
Evgeny <evgeny.hyvarinen@tietoevry.com>
2023-10-15 14:38:13 +0300

    swagger integration

--- geobackend/geobackend/urls.py
+++ geobackend/geobackend/urls.py
@@ -15,11 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+import os
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="User-pointing-map API",
+        default_version='v1',
+        description="Welcome to create a points with other users",
+        contact=openapi.Contact(email=os.environ.get("DJANGO_ADMIN_EMAIL")),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path("api/", include("map_app.urls", namespace="locations")),
 	path("api/users/", include("users.urls", namespace="users")),
+	path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
